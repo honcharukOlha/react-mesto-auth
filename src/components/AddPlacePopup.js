7e089189b9be1df0125610be243450b8bc509bbe
Goncharuk <OlyaGoncharuk@yandex.ru>
2021-04-25 22:40:33 +0300

    итог

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PopupWithForm from './PopupWithForm.js';
-import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
     const [name, setName] = React.useState('');
     const [link, setLink] = React.useState('');
-    // Подписка на контекст
-    const currentUser = React.useContext(CurrentUserContext);
 
     React.useEffect(() => {
         setName('');
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PopupWithForm from './PopupWithForm.js';
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
